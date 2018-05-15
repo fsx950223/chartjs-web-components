12329c48d75eaecce2ec52c018daa41c9a09e463
fsx950223 <fsx950223@outlook.com>
2018-05-15 11:39:07 +0800

    feat(readme): add readme.md

--- src/my-app.js
+++ src/my-app.js
@@ -7,25 +7,26 @@ The complete set of contributors may be found at http://polymer.github.io/CONTRI
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
-
 import { LitElement, html } from '@polymer/lit-element';
 import './base.js';
-
 class MyApp extends LitElement {
-  data={
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [{
-        label: "My First dataset",
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
-    }]
-  }
-  _render() {
-    return html`
-        <base-chart type="line" data="${this.data}" options="${{}}"></base-chart>
+    constructor() {
+        super(...arguments);
+        this.data = {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [{
+                    label: "My First dataset",
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: [0, 10, 5, 2, 20, 30, 45],
+                }]
+        };
+    }
+    _render() {
+        return html `
+      <base-chart type="line" data="${this.data}" options="${{}}"></base-chart>
     `;
-  }
+    }
 }
-
 window.customElements.define('my-app', MyApp);
+//# sourceMappingURL=my-app.js.map
